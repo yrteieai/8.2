ab5ce0694f163e8493821b8e4d53101584c786d5
Anya Stefurak <anyutastefurak@gmail.com>
2024-10-27 16:42:38 +0200

    first master commit

---
+++ 8.2/8.2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Функція перевіряє, чи є символ літерою
+bool IsLetter(char ch) {
+    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+}
+
+// Функція, яка робить першу літеру слова великою, якщо це слово стоїть після крапки
+string CapitalizeAfterPeriod(const string& s) {
+    string result = s;
+    bool capitalizeNext = false; // Прапор для перетворення першої літери після крапки
+
+    for (size_t i = 0; i < result.length(); ++i) {
+        if (result[i] == '.') {
+            capitalizeNext = true; // Встановити прапор після крапки
+        }
+        else if (capitalizeNext && IsLetter(result[i])) {
+            result[i] = toupper(result[i]); // Перетворення на велику літеру
+            capitalizeNext = false; // Скидання прапора після першої літери
+        }
+        else if (result[i] != ' ' && result[i] != '.') {
+            capitalizeNext = false; // Скидання прапора, якщо зустріли символ не після крапки
+        }
+    }
+    return result;
+}
+
+int main() {
+    string input;
+    cout << "Enter string:" << endl;
+    getline(cin, input);
+
+    string result = CapitalizeAfterPeriod(input);
+    cout << "odified string: " << result << endl;
+
+    return 0;
+}
---
+++ UnitTest_8.2/UnitTest_8.2.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../8.2/8.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest82
+{
+	TEST_CLASS(UnitTest82)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+	
+			string input = "Hello. this is a test.";
+			string expected = "Hello. This is a test.";
+			Assert::AreEqual(expected, CapitalizeAfterPeriod(input), L"The first letter after a period should be capitalized");
+
+			input = "Hello.   how are you?";
+			expected = "Hello.   How are you?";
+			Assert::AreEqual(expected, CapitalizeAfterPeriod(input), L"The word after the period with spaces should start with a capital letter");
+
+			input = "this is a test without periods";
+			expected = "this is a test without periods";
+			Assert::AreEqual(expected, CapitalizeAfterPeriod(input), L"A string without periods should not change");
+
+			input = "Here is a sentence. here is another one. and another.";
+			expected = "Here is a sentence. Here is another one. And another.";
+			Assert::AreEqual(expected, CapitalizeAfterPeriod(input), L"The first letter of each word after a period should be capitalized");
+
+			input = ".starting with a period.";
+			expected = ".Starting with a period.";
+			Assert::AreEqual(expected, CapitalizeAfterPeriod(input), L"The first word after an initial period should start with a capital letter");
+		}
+	};
+}
